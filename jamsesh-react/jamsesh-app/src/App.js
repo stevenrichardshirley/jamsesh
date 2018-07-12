db34c705cd819dd5b44898722aaff7b6e90453d9
Christopher Faison <pmatrixmedia@gmail.com>
2018-07-11 21:02:02 -0500

    Login added, fixed routes and added firebase database

--- jamsesh-react/jamsesh-app/src/App.js
+++ jamsesh-react/jamsesh-app/src/App.js
@@ -18,9 +18,9 @@ import Svg,{
 } from 'svgs';
 import './App.css';
 import FrameOutline from './frame/frame.js';
-import HomeAnimation from './home/home.js';
-import InstrumentsSelector from './instruments/instruments.js';
-import MusiciansBrowser from './musicians/musicians.js';
+import Home from './home/home.js';
+import Instruments from './instruments/instruments.js';
+import Musicians from './musicians/musicians.js';
 import Jumbotron from "./components/Jumbotron/Jumbotron.js";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class App extends Component {
@@ -31,9 +31,9 @@ class App extends Component {
           <Jumbotron />
           <Router>
               <div>
-                <Route path="/home" component={HomeAnimation} />
-                <Route path="/instruments" component={InstrumentsSelector} />
-                <Route path="/musicians" component={MusiciansBrowser} />
+                <Route path="/home" component={Home} />
+                <Route path="/instruments" component={Instruments} />
+                <Route path="/musicians" component={Musicians} />
               </div>
             </Router>
         </FrameOutline>
