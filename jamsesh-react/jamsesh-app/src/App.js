5c52a72864c9c374ac77faeef72acdcef2255bcf
merritt Webb <merrittwebb@merritts-MacBook-Pro.local>
2018-07-16 19:55:17 -0500

    checkbox added

--- jamsesh-react/jamsesh-app/src/App.js
+++ jamsesh-react/jamsesh-app/src/App.js
@@ -5,6 +5,7 @@ import Frame from './frame/frame.js';
 import Home from './home/home.js';
 import Instruments from './instruments/instruments.js';
 import Musicians from './musicians/musicians.js';
+import Checkbox from './components/checkbox/checkbox.js';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
                 <Route path="/home" component={Home} />
                 <Route path="/instruments" component={Instruments} />
                 <Route path="/musicians" component={Musicians} />
+                <Route path="/signedIn" component ={Checkbox} />
               </div>
             </Router>
         </Frame>
