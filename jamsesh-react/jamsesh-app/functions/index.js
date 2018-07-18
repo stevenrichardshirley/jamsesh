212aa7d34c38340d1ed11bbd61defa343a81b096
Christopher Faison <pmatrixmedia@gmail.com>
2018-07-18 00:51:58 -0500

    added firebase functions and added req, res to cloud

--- jamsesh-react/jamsesh-app/functions/index.js
+++ jamsesh-react/jamsesh-app/functions/index.js
@@ -1,5 +1,75 @@
 const functions = require('firebase-functions');
 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+      // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+      return res.redirect(303, snapshot.ref.toString());
+    });
+  });
+
+function handleClientLoad() {
+    // Loads the client library and the auth2 library together for efficiency.
+    // Loading the auth2 library is optional here since `gapi.client.init` function will load
+    // it if not already loaded. Loading it upfront can save one network request.
+    gapi.load('client:auth2', initClient);
+  }
+
+  function initClient() {
+    // Initialize the client with API key and People API, and initialize OAuth with an
+    // OAuth 2.0 client ID and scopes (space delimited string) to request access.
+    gapi.client.init({
+        apiKey: 'AIzaSyDYc_Phs9I-6OQYRdcvmPPi9AJXNOYiE1I',
+        discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
+        clientId: '500614187735-ijct501n3sdqnrjkh2q0prknkkoak0ov.apps.googleusercontent.com',
+        scope: 'profile'
+    }).then(function () {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+      // Handle the initial sign-in state.
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    });
+  }
+
+  function updateSigninStatus(isSignedIn) {
+    // When signin status changes, this function is called.
+    // If the signin status is changed to signedIn, we make an API call.
+    if (isSignedIn) {
+      makeApiCall();
+    }
+  }
+
+  function handleSignInClick(event) {
+    // Ideally the button should only show up after gapi.client.init finishes, so that this
+    // handler won't be called before OAuth is initialized.
+    gapi.auth2.getAuthInstance().signIn();
+  }
+
+  function handleSignOutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+  }
+
+  function makeApiCall() {
+    // Make an API call to the People API, and print the user's given name.
+    gapi.client.people.people.get({
+      'resourceName': 'people/me',
+      'requestMask.includeField': 'person.names'
+    }).then(function(response) {
+      console.log('Hello, ' + response.result.names[0].givenName);
+    }, function(reason) {
+      console.log('Error: ' + reason.result.error.message);
+    });
+  }
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
